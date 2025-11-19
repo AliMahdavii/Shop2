deb39f64c2e627ada8c2d8f3db670b00789a943a
Ali.Mahdavi <84a.mahdaviniya@gmail.com>
2025-11-19 11:08:59 +0330

    Update products.js

--- scripts/products.js
+++ scripts/products.js
@@ -120,6 +120,297 @@ export const products = [
         category: "کوله پشتی",
         description : "کوله‌پشتی جادار و بادوام با طراحی کاربردی. مناسب دانشگاه، سفر یا استفاده روزمره با دوخت مستحکم و ظاهر مدرن.",
         id: "108"
+    },
+    {
+        name: "کت Detroit",
+        price: 3800000,
+        percent: 0,
+        mainImage: "images/3/IMG_1908.AVIF",
+        gallery: [
+            "images/3/IMG_1909.AVIF",
+            "images/3/IMG_1910.AVIF",
+            "images/3/IMG_1911.AVIF",
+            "images/3/IMG_1912.AVIF",
+            "images/3/IMG_1913.AVIF",
+            "images/3/IMG_1914.AVIF"
+        ],
+        category: "کت",
+        description : "کتی شیک و خوش‌فرم با برش کلاسیک که هم برای مجالس رسمی و هم استایل نیمه‌رسمی عالیه. ترکیب وقار و سادگی.",
+        id: "106"
+    },
+    {
+        name: "شلوار Brandon",
+        price: 2100000,
+        percent: 0,
+        mainImage: "images/7/IMG_1940.AVIF",
+        gallery: [
+            "images/7/IMG_1940.AVIF",
+            "images/7/IMG_1941.AVIF",
+            "images/7/IMG_1942.AVIF",
+            "images/7/IMG_1943.AVIF"
+        ],
+        category: "شلوار",
+        description : "شلواری راحت و خوش‌دوخت، مناسب استفاده روزمره و استایل خیابانی. ترکیب عالی از سادگی و راحتی برای هر موقعیتی.",
+        id: "107"
+    },
+    {
+        name: "کلاه Carhatt",
+        price: 710000,
+        percent: 0,
+        mainImage: "images/9/IMG_1961.AVIF",
+        gallery: [
+            "images/9/IMG_1962.AVIF"
+        ],
+        category: "کلاه",
+        description : "کلاهی سبک و راحت با جنس باکیفیت، مناسب روزهای آفتابی یا تکمیل استایل اسپرت. هم کاربردی هم فشن.",
+        id: "109"
+    },
+    {
+        name: "شلوارک Rylan",
+        price: 1200000,
+        percent: 0,
+        mainImage: "images/10/IMG_1955.AVIF",
+        gallery: [
+            "images/10/IMG_1956.AVIF",
+            "images/10/IMG_1957.AVIF",
+            "images/10/IMG_1958.AVIF",
+            "images/10/IMG_1959.AVIF"
+        ],
+        category: "شلوارک",
+        description : "شلوارکی خنک و سبک، مناسب روزهای گرم تابستان و فعالیت‌های روزمره. راحتی همراه با استایل مینیمال.",
+        id: "110"
+    },
+    {
+        name: " کیف دستی Bayfield",
+        price: 800000,
+        percent: 0,
+        mainImage: "images/11/IMG_1945.AVIF",
+        gallery: [
+            "images/11/IMG_1946.AVIF",
+            "images/11/IMG_1947.AVIF",
+            "images/11/IMG_1948.AVIF",
+            "images/11/IMG_1949.AVIF"
+        ],
+        category: "کیف دستی ",
+        description : "کیف دستی ظریف با طراحی مینیمال و دوخت دقیق، مناسب برای استفاده روزمره یا موقعیت‌های خاص. سبک و در عین حال جادار.",
+        id: "111"
+    },
+    {
+        name: "کلاه Carhatt",
+        price: 770000,
+        percent: 0,
+        mainImage: "images/12/IMG_1951.AVIF",
+        gallery: [
+            "images/12/IMG_1952.AVIF",
+            "images/12/IMG_1953.AVIF",
+        ],
+        category: "کلاه",
+        description : "کلاهی سبک و راحت با جنس باکیفیت، مناسب روزهای آفتابی یا تکمیل استایل اسپرت. هم کاربردی هم فشن.",
+        id: "112"
+    },
+    {
+        name: " دورس Detroit",
+        price: 1800000,
+        percent: 0,
+        mainImage: "images/13/IMG_2633.WEBP",
+        gallery: [
+            "images/13/IMG_2634.WEBP",
+            "images/13/IMG_2635.WEBP",
+            "images/13/IMG_2636.WEBP",
+            "images/13/IMG_2637.WEBP",
+        ],
+        category: "پیراهن",
+        description : "کیف دستی ظریف با طراحی مینیمال و دوخت دقیق، مناسب برای استفاده روزمره یا موقعیت‌های خاص. سبک و در عین حال جادار.",
+        id: "113"
+    },
+    {
+        name: "سویشرت کلاه‌دار ویستا",
+        price: 1800000,
+        percent: 0,
+        mainImage: "images/15/IMG_2646.WEBP",
+        gallery: [
+            "images/15/IMG_2647.WEBP",
+            "images/15/IMG_2648.WEBP",
+            "images/15/IMG_2649.WEBP",
+            "images/15/IMG_2650.WEBP",
+        ],
+        category: "پیراهن",
+        description : "سویشرت راحت و گرم با طراحی مینیمال، مناسب استفاده روزمره و هوای خنک.",
+        id: "115"
+    },
+    {
+        name: "ژاکت سویتی اسپرت",
+        price: 2800000,
+        percent: 0,
+        mainImage: "images/16/IMG_2652.WEBP",
+        gallery: [
+            "images/16/IMG_2653.WEBP",
+            "images/16/IMG_2654.WEBP",
+            "images/16/IMG_2655.JPG",
+            "images/16/IMG_2656.WEBP",
+            "images/16/IMG_2657.WEBP",
+            "images/16/IMG_2658.WEBP",
+            "images/16/IMG_2659.WEBP",
+        ],
+        category: "ژاکت",
+        description : "ژاکت سبک و قابل تنفس برای فعالیت روزانه یا ورزش، ترکیب استایل و راحتی.",
+        id: "116"
+    },
+    {
+        name: "شلوار کارگو کول (پارچه توییل رنگ‌ شده)",
+        price: 2800000,
+        percent: 0,
+        mainImage: "images/17/IMG_2661.WEBP",
+        gallery: [
+            "images/17/IMG_2662.WEBP",
+            "images/17/IMG_2663.WEBP",
+            "images/17/IMG_2664.WEBP",
+            "images/17/IMG_2665.WEBP",
+            "images/17/IMG_2666.WEBP",
+        ],
+        category: "شلوار",
+        description : "شلوار چندجیبه با جنس توییل، رنگ‌شده پس از دوخت برای ظاهر خاص و کهنه‌نما.",
+        id: "117"
+    },
+    {
+        name: "کلاه Trucker",
+        price: 400000,
+        percent: 0,
+        mainImage: "images/19/IMG_2676.WEBP",
+        gallery: [
+            "images/19/IMG_2677.WEBP",
+        ],
+        category: "کلاه",
+        description : "کلاه پشت‌توری با لوگوی برجسته، استایل خیابانی و کژوال.",
+        id: "119"
+    },
+    {
+        name: "کلاه تترا",
+        price: 800000,
+        percent: 0,
+        mainImage: "images/20/IMG_2679.WEBP",
+        gallery: [
+            "images/20/IMG_2680.WEBP",
+        ],
+        category: "کلاه",
+        description : "کلاه ساده و اسپرت با طراحی چهار بخشی کلاسیک.",
+        id: "120"
+    },
+    {
+        name: "کلاه Pillbox",
+        price: 800000,
+        percent: 0,
+        mainImage: "images/22/IMG_2688.WEBP",
+        gallery: [
+            "images/22/IMG_2689.WEBP",
+            "images/22/IMG_2690.WEBP",
+        ],
+        category: "کلاه",
+        description : "مدل الهام‌گرفته از استایل نظامی و قدیمی، اغلب به‌صورت استوانه‌ای شکل.",
+        id: "122"
+    },
+    {
+        name: "کلاه بافتنی اوهایو",
+        price: 800000,
+        percent: 0,
+        mainImage: "images/23/IMG_2692.WEBP",
+        gallery: [
+            "images/23/IMG_2693.JPG",
+            "images/23/IMG_2694.WEBP",
+        ],
+        category: "کلاه",
+        description : "کلاه بافتنی ساده و گرم با ظاهر کلاسیک، مناسب روزهای سرد.",
+        id: "123"
+    },
+    {
+        name: "کلاه بافتنی Gabe",
+        price: 800000,
+        percent: 0,
+        mainImage: "images/24/IMG_2696.WEBP",
+        gallery: [
+            "images/24/IMG_2697.WEBP",
+            "images/24/IMG_2698.WEBP",
+        ],
+        category: "کلاه",
+        description : "کلاه بافتنی نرم‌تر و سبک‌تر، مناسب استایل روزمره شهری.",
+        id: "124"
+    },
+    {
+        name: "پیراهن‌ ژاکت مخمل کبریتی Reynold",
+        price: 800000,
+        percent: 0,
+        mainImage: "images/25/IMG_2700.WEBP",
+        gallery: [
+            "images/25/IMG_2701.WEBP",
+            "images/25/IMG_2702.WEBP",
+            "images/25/IMG_2703.WEBP",
+            "images/25/IMG_2704.WEBP",
+            "images/25/IMG_2705.WEBP",
+            "images/25/IMG_2706.WEBP",
+        ],
+        category: "ژاکت",
+        description : "ترکیب پارچه مخمل کبریتی و طراحی کت، مناسب پاییز با حس کلاسیک.",
+        id: "125"
+    },
+    {
+        name: "تیشرت راه‌راه Malone",
+        price: 800000,
+        percent: 0,
+        mainImage: "images/26/IMG_2708.WEBP",
+        gallery: [
+            "images/26/IMG_2709.WEBP",
+            "images/26/IMG_2710.WEBP",
+            "images/26/IMG_2711.WEBP",
+            "images/26/IMG_2712.WEBP",
+        ],
+        category: "پیراهن",
+        description : "تیشرت راه‌راه با رنگ‌های متضاد، استایل وینتج و خیابانی.",
+        id: "126"
+    },
+    {
+        name: "پیراهن راگبی راه‌راه Malone",
+        price: 800000,
+        percent: 0,
+        mainImage: "images/27/IMG_2714.WEBP",
+        gallery: [
+            "images/27/IMG_2715.WEBP",
+            "images/27/IMG_2716.WEBP",
+            "images/27/IMG_2717.WEBP",
+            "images/27/IMG_2718.WEBP",
+        ],
+        category: "پیراهن",
+        description : "پیراهن ضخیم‌تر با یقه سفید و طرح راگبی کلاسیک، مناسب استایل دهه ۹۰.",
+        id: "127"
+    },
+    {
+        name: "تیشرت آستین‌بلند بافت Vista Waffle",
+        price: 800000,
+        percent: 0,
+        mainImage: "images/28/IMG_2720.WEBP",
+        gallery: [
+            "images/28/IMG_2721.WEBP",
+            "images/28/IMG_2722.WEBP",
+            "images/28/IMG_2723.JPG",
+            "images/28/IMG_2724.JPG",
+        ],
+        category: "پیراهن",
+        description : "بافت وافل (شانه‌عسل) برای گرمای بیشتر و ظاهر تکسچردار، مناسب هوای سردتر.",
+        id: "128"
+    },
+    {
+        name: "تیشرت Benton",
+        price: 800000,
+        percent: 0,
+        mainImage: "images/29/IMG_2726.WEBP",
+        gallery: [
+            "images/29/IMG_2727.WEBP",
+            "images/29/IMG_2728.WEBP",
+            "images/29/IMG_2729.WEBP",
+            "images/29/IMG_2730.WEBP",
+        ],
+        category: "پیراهن",
+        description : "تیشرت ساده و تمیز با طراحی مینیمال، مناسب استایل‌های روزانه.",
+        id: "129"
     }
     
 ];
